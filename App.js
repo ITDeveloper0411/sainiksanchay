39ac9eb6a94159acd2b8e39dd1d7aa83bfcd6fa6
Hitesh Rawat <itdeveloperasc@gmail.com>
2025-09-14 19:02:54 +0530

    code push

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MainNavigator from './src/navigation/MainNavigator';
 import { Provider } from 'react-redux';
 import store from './src/store/store';
-
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SpInAppUpdates, { IAUUpdateKind } from 'sp-react-native-in-app-updates';
 
 function App() {
+
+  useEffect(() => {
+    const inAppUpdates = new SpInAppUpdates(false); // Set to true for debug logs
+    const checkForUpdates = async () => {
+      try {
+        const result = await inAppUpdates.checkNeedsUpdate();
+        if (result.shouldUpdate) {
+          const updateOptions = {
+            updateType: IAUUpdateKind.IMMEDIATE,
+          };
+          inAppUpdates.startUpdate(updateOptions);
+        }
+      } catch (e) {
+        console.log('Error checking for updates:', e);
+      }
+    };
+    checkForUpdates();
+  }, []);
+
   return (
     <Provider store={store}>
       <SafeAreaProvider>
